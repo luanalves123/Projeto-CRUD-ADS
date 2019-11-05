bbe07943afdb056f1db2c2dc10ef0c3bdea19bd2
Luan <luan24823@gmail.com>
2019-11-05 20:56:35 -0200

    CRUD Inicial

---
+++ ProjetoCRUD/src/univs/edu/usuario/Usuario.java
@@ -0,0 +1,71 @@
+
+package univs.edu.usuario;
+
+public class Usuario {
+
+    private int idUsuario;
+    
+    private String nomeUsuario;
+    
+    private String login;
+    
+    private String senha;
+
+    /**
+     * @return the idUsuario
+     */
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    /**
+     * @param idUsuario the idUsuario to set
+     */
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    /**
+     * @return the nomeUsuario
+     */
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    /**
+     * @param nomeUsuario the nomeUsuario to set
+     */
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+   
+}
